d30ce4252e4a9953f334a6816aa7bb658dd73eb2
meloviliju <ritterderschwerter178@gmail.com>
2023-05-05 07:27:54 +0900

    implement multiple person

--- netxiju.js
+++ netxiju.js
@@ -1,3 +1,4 @@
+"use strict";
 const getXiju = () => {
     let res = 0;
     for (let i = 0; i < 6; i++) {
@@ -6,34 +7,45 @@ const getXiju = () => {
     return res;
 };
 
-const oneperson = () => {
-    const two = [0, 0, 2];
-    const three = [0, 0, 0, 3];
-    const alcoholTable = [
-        ...two,
-        ...two,
-        ...two,
-        ...two,
-        ...two,
-        ...three,
-        ...three,
-        ...three,
-        ...three,
-    ];
-    const person = {
+const two = [0, 0, 2];
+const three = [0, 0, 0, 3];
+const alcoholTable = [
+    0,
+    ...two,
+    ...two,
+    ...two,
+    ...two,
+    ...two,
+    ...three,
+    ...three,
+    ...three,
+    ...three,
+];
+
+const simulate = (person_num) => {
+    const persons = Array.from({ length: person_num }, () => ({
         alcohol: 0,
-        position: 0
-    };
+        position: 0,
+        hasGoaled: false,
+    }));
     while (true) {
-        let xiju = getXiju();
-        if (person.position + xiju >= alcoholTable.length) {
-            continue;
-        }
-        person.position += xiju;
-        person.alcohol += alcoholTable[person.position];
-        if (person.position === alcoholTable.length - 1) {
-            break;
+        for (let i = 0; i < person_num; i++) {
+            const person = persons[i];
+            if (person.hasGoaled) {
+                continue;
+            }
+            let xiju = getXiju();
+            if (person.position + xiju >= alcoholTable.length) {
+                continue;
+            }
+            person.position += xiju;
+            person.alcohol += alcoholTable[person.position];
+            if (person.position === alcoholTable.length - 1) {
+                person.hasGoaled = true;
+            }
+            if (persons.every(p => p.hasGoaled)) {
+                return persons;
+            }
         }
     }
-    return person.alcohol;
 }
